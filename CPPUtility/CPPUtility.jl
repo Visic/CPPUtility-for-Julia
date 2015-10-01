a9a4332934a765517a97af82e951e6028e47724c
Andrew <a.frailing@gmail.com>
2015-10-01 13:03:11 -0400

    Added the KEYCODES enumeration

--- CPPUtility/CPPUtility.jl
+++ CPPUtility/CPPUtility.jl
@@ -1,4 +1,5 @@
 module CPPUtility
+using KEYCODES
 export getkeystate, get_async_key_state, set_console_cursor_pos, get_console_cursor_pos, getlasterror, clearscreen, set_console_cursor_visibility, getconsoledims
 
 const dllName = "CPPUtility.dll"
@@ -8,8 +9,8 @@ immutable Coord
     Y::Int16
 end
 
-getkeystate(keycode::Integer) = ccall((:getKeyState, dllName), Int8, (Int32,), keycode)
-get_async_key_state(keycode::Integer) = ccall((:getAsyncKeyState, dllName), Int8, (Int32,), keycode)
+getkeystate(keycode::KEYCODES.KEYCODE) = ccall((:getKeyState, dllName), Int8, (Int32,), keycode)
+get_async_key_state(keycode::KEYCODES.KEYCODE) = ccall((:getAsyncKeyState, dllName), Int8, (Int32,), keycode)
 set_console_cursor_pos(x::Integer, y::Integer) = ccall((:setConsoleCursorPos, dllName), Int64, (Int16, Int16), x, y)
 getlasterror() = ccall((:getLastError, dllName), Int64, ())
 clearscreen() = ccall((:clearScreen, dllName), Int32, ())
@@ -24,5 +25,4 @@ function getconsoledims()
     coord = ccall((:getConsoleDims, dllName), Coord, ())
     (coord.X, coord.Y)
 end
-
 end
